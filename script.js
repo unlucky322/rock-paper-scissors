5e02677b13ff59defda873c425650f40c728b89e
Nikita <147027521+unlucky322@users.noreply.github.com.>
2024-04-18 19:06:43 +0300

    making a function to play 1 round

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-function getComputerChioce() {
-    let value = values[Math.floor(Math.random() * values.length)];
-    return value
+let computerSelection;
+function getComputerChoice() {
+    computerSelection = values[Math.floor(Math.random() * values.length)];
+    return computerSelection
 }
 
-values = ['Rock', 'Paper', 'Scissors'];
+values = ['rock', 'paper', 'scissors'];
+let playerSelection;
+function getPlayerChoice() {
+    playerSelection = prompt("Rock, paper, scissors? What will you choice?")
+    return playerSelection.toLowerCase()
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie"
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            computerScore++
+            return "Computer won! " + "Score: " + playerScore + ":" + computerScore
+        }
+        else {
+            playerScore++
+            return "Player won! " + "Score: " + playerScore + ":" + computerScore
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            computerScore++
+            return "Computer won! " + "Score: " + playerScore + ":" + computerScore
+        } else {
+            playerScore++
+            return "Player won! " + "Score: " + playerScore + ":" + computerScore
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            computerScore++
+            return "Computer won! " + "Score: " + playerScore + ":" + computerScore
+        } else {
+            playerScore++
+            return "Player won! " + "Score: " + playerScore + ":" + computerScore
+        }
+    }
+}
